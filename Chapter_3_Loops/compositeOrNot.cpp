f5ed52715c5ad4985530eddc7f860d5314bbfc56
lastcookiee <kunal7838rekt@gmail.com>
2025-08-15 12:45:29 +0530

    Re editing the composite code for practice

--- Chapter_3_Loops/compositeOrNot.cpp
+++ Chapter_3_Loops/compositeOrNot.cpp
@@ -3,16 +3,10 @@ using namespace std;
 
 int main(){
     int n;
-    cout<<"enter a number: ";
+    cout<<"Enter a number: ";
     cin>>n;
     bool flag = true;
-
-    for(int i=2; i <= n/2 ; i++){
-        if(n%i==0){
-            flag = false;
-            break;
-        }
+    for(int i=2 ; i=n/2 ;i++){
+        
     }
-    if(flag==false) cout<<"composite";
-    else cout<<"not composite";
 }
